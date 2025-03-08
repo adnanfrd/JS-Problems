5ee1754ac44b47f8acab941b05ff4e591c10286e
adnanfrd <adnanmadhar74@gmail.com>
2025-03-08 14:28:58 +0500

    recursive function

--- random.js
+++ random.js
@@ -8,5 +8,44 @@ let car = {
 //  car.color = 'red'; adding new value...
 // let updatedCAr = {...car, color:"red"};
 // let updatedCar = Object.assign({},car, {color:"Red"});
-  console.log(updatedCar);
+//   console.log(car);
   
+  //Recursive function...
+
+  //Reacursive function is a function that is called itself somewhere in the body of the function. it's repeat calling untill the desired output is achieved.
+
+  function recursiveFunction(n){
+    if(n === 0){
+        console.log("Task Completed!"); 
+        return; 
+    } else{
+        console.log("I'm doing something!");
+        recursiveFunction(n-1)
+    }
+  }
+
+//   recursiveFunction(10);
+function findFactorial(num) {
+    if (num === 0) return 1
+    let factorial = num * findFactorial(num - 1)
+    return factorial;
+
+  }
+  
+  findFactorial(5) 
+
+  //Recursive function has three parts :
+  // the function definition      2. the base condition   3. the recursive call
+
+  function createHello(){
+       return function newFunc(){
+        return "Hello Wolrd";
+       }
+  }
+  createHello();
+
+  //Store  in redux : the global state of your application stored in an object tree within a single store.
+  //Action: the only way to change the state is to emit an action, an object describing what happened.
+  // Reducers: To specify how the state tree is transformed by actions, you write pure reducers.
+  //Reducers are just pure functions that take the previous state and an action, and return the next state.
+
